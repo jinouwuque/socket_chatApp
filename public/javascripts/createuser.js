0a81dce21ff6b67e847489fe9e69fa8a8a49d491
jinouwuque <ee2win@gmail.com>
2015-05-14 23:56:04 -0400

    icon
    icon

--- public/javascripts/createuser.js
+++ public/javascripts/createuser.js
@@ -1,9 +1,8 @@
 $(document).ready(function(){
     var socket = io("http://localhost:3000");
     $('#createUserBtn').on('click', function(){
-        debugger;
         var username = $('#usernameBtn').val();
-        socket.emit('new message', username);
+        socket.emit('add new user', username);
     });
     socket.on('response new message', function(){
         console.log('response new message aha!');

2023d04c9f47ce45f40158ed7fa131654ed08d9e
jinouwuque <ee2win@gmail.com>
2015-05-18 01:08:25 -0400

    add parser cookie
    cookie

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var chat = require('./routes/chat');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.locals.pretty = true;
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/chatting', chat);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ middlewares/socket_app.js
@@ -0,0 +1,66 @@
+var numUsers = 0;
+var onlineusers = {};
+var colorindex = 0;
+
+function socket_app(io){
+    io.on('connection', function(socket){
+        for(var i = 0 ; i < 3; i ++){
+            socket.send({
+                iconid: 1,
+                username: 'node zj',
+                msg: 'gaga gagaga!'
+            });
+        }
+
+        socket.on('new message', function(data){
+            var senderinfo = onlineusers[socket.id];
+            debugger;
+            for(var i = 0; i < data.dest.length; i ++){
+                socket.to(data.dest[i]).emit('group msg', {
+                    iconid: senderinfo.iconid,
+                    username: senderinfo.username,
+                    msg : data.msg
+                });
+            }
+        });
+        socket.on('add new user', function (data) {
+            // we tell the client to execute 'new message'
+            onlineusers[socket.id] = {
+                username: data.username,
+                iconid : data.iconid,
+                userid: socket.id,
+                colorid : colorindex++
+            };
+            numUsers ++;
+            socket.broadcast.emit('inf_updateUsers', onlineusers);
+            socket.emit('ack_conn', socket.id);
+        });
+
+        // when the client emits 'typing', we broadcast it to others
+        socket.on('typing', function () {
+            socket.broadcast.emit('typing', {
+                username: socket.username
+            });
+        });
+
+        // when the client emits 'stop typing', we broadcast it to others
+        socket.on('stop typing', function () {
+            socket.broadcast.emit('stop typing', {
+                username: socket.username
+            });
+        });
+
+        // when the user disconnects.. perform this
+        socket.on('disconnect', function () {
+            delete onlineusers[socket.id];
+            --numUsers;
+            // echo globally that this client has left
+            socket.broadcast.emit('user left', {
+                username: socket.username,
+                numUsers: numUsers
+            });
+        });
+    });
+}
+
+module.exports = socket_app;
